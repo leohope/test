7073ee03c26685a02fdc83f8972a94cb2b3a3ec9
Nadia <leonna.leohope@gmail.com>
2025-03-11 22:00:34 +0300

    fix get in fixture

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -1,26 +1,20 @@
-import shutil
-
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-import tempfile
 from selenium.webdriver.common.by import By
+
 
 @pytest.fixture
 def driver():
-    chrome_options = Options()
+    driver = webdriver.Chrome()
 
-    user_data_dir = tempfile.mkdtemp()
-    chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
-
-    driver = webdriver.Chrome(options=chrome_options)
-    driver.get("https://practicetestautomation.com/practice-test-login/")
-    driver.maximize_window()
+    driver.implicitly_wait(10)
     yield driver
     driver.quit()
-    shutil.rmtree(user_data_dir)
+
 
 def test_login(driver):
+    driver.get("https://practicetestautomation.com/practice-test-login/")
+
     driver.find_element(By.ID, "username").send_keys("student")
     driver.find_element(By.ID, "password").send_keys("Password123")
     driver.find_element(By.ID, "submit").click()

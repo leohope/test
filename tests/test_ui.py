37d37edc2a239f790a1154dc7269b3b0e158fd58
Nadia <leonna.leohope@gmail.com>
2025-03-11 22:08:29 +0300

    fix sandbox/headless

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -1,11 +1,16 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome()
+    chrome_options = Options()
+    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--no-sandbox")
+
+    driver = webdriver.Chrome(options=chrome_options)
 
     driver.implicitly_wait(10)
     yield driver

1275f1f3862d432558e05d4589c6407668d6be62
Nadia <leonna.leohope@gmail.com>
2025-03-11 21:49:02 +0300

    use temp dir

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -1,14 +1,22 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import tempfile
 from selenium.webdriver.common.by import By
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome()
+    chrome_options = Options()
+
+    user_data_dir = tempfile.mkdtemp()
+    chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
+
+    driver = webdriver.Chrome(options=chrome_options)
     driver.get("https://practicetestautomation.com/practice-test-login/")
     driver.maximize_window()
     yield driver
     driver.quit()
+
 
 def test_login(driver):
     driver.find_element(By.ID, "username").send_keys("student")

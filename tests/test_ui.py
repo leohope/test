629d43a5025cfc12cc52c27726d81c237e5b2ce2
Nadia <leonna.leohope@gmail.com>
2025-03-12 20:52:42 +0300

    Add Allure report

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -1,7 +1,7 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
 
 @pytest.fixture
